d9834b4bd6d762c3ad82bb1867df5de01adc21e7
Iluminatic-Mokd <std.67122420205@ubru.ac.th>
2026-02-14 15:07:25 +0700

    modify

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.db'
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.secret_key = 'sdlflkposkrofkpsldas'
+
+from epl import models,routes
--- epl/models.py
+++ epl/models.py
@@ -7,7 +7,7 @@ class Club(db.Model):
     __tablename__ = 'clubs'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
-    city: Mapped[str] = mapped_column(String(100), nullable=Ture)
+    city: Mapped[str] = mapped_column(String(100), nullable=True)
     stadium: Mapped[str] = mapped_column(String(100), nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
     logo: Mapped[str] = mapped_column(String(200), nullable=True)
@@ -22,8 +22,11 @@ class Player(db.Model):
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     position: Mapped[str] = mapped_column(String(50), nullable=False)
     nationality: Mapped[str] = mapped_column(String(50), nullable=False)
+    goal: Mapped[int] = mapped_column(Integer, nullable=True, default=0)
+    squad_no: Mapped[int] = mapped_column(Integer, nullable=True)
     img: Mapped[str] = mapped_column(String(200), nullable=True)
     club_id: Mapped[int] = mapped_column(Integer, ForeignKey('clubs.id'))
+    
     club: Mapped["Club"] = relationship(back_populates="players")
     
     def __repr__(self) -> str:
